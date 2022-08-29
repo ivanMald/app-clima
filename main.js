0970d64c210099f642f7d8f4d477185487aef2ac
Ivan Maldonado <ivanvalentinmaldonado2014@hotmail.com>
2022-08-29 17:09:18 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+let weather = {
+    "apiKey": "e3634418d561e8737a91588f9a82fbaf",
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + city 
+        +"&units=metric&appid=" 
+        + this.apiKey
+    )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data) );
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Clima en " + name
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humedad: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Viento a: " + speed + " m/s";
+        document.querySelector(".weather").classList.remove("loading")
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/? "+ name +"')"
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function (){
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+})
+
+weather.fetchWeather("Buenos aires");
